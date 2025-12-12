6868a2b9cc5c88e10907ad0b779bcf77024d43d9
qa-tech-training <agray@qa.com>
2025-12-12 06:38:58 +0000

    quality-of-life improvement to setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ export TF_VAR_gcp_project=$(gcloud config get project)
 export GCS_BUCKET="qadevoprac3-capstone-bucket-$RANDOM-$RANDOM"
 printf "Creating Bucket\n\n"
 gcloud storage buckets create gs://$GCS_BUCKET --location=europe-west1
-printf "\n\nCreated Bucket: %s" $GCS_BUCKET
+printf "\n\nCreated Bucket: %s\n" $GCS_BUCKET
 wget https://raw.githubusercontent.com/qa-tech-training/qadevoprac3-capstone-lab-starter/refs/heads/main/jenkins.tf
 terraform init > terraform.log 2>&1
 terraform apply -auto-approve >> terraform.log 2>&1
