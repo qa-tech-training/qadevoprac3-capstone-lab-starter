0c06729fea221f70df606555fcf189277f6170e9
qa-tech-training <agray@qa.com>
2026-01-16 09:07:10 +0000

    Update jenkins.tf

--- jenkins.tf
+++ jenkins.tf
@@ -40,7 +40,7 @@ resource "google_compute_instance" "jenkins_server" {
   curl -L "$(curl -s https://api.github.com/repos/tenable/terrascan/releases/latest | grep -o -E "https://.+?_Linux_x86_64.tar.gz")" > terrascan.tar.gz
   tar -xf terrascan.tar.gz terrascan && rm terrascan.tar.gz
   sudo install terrascan /usr/local/bin && rm terrascan
-  curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2026.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
   echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
   apt-get update
   apt-get install -y jenkins
